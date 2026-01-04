cb91fe6ddefedb661aa664533ff3faaac21b6e1f
dangnk <nguyenkhoad016@gmail.com>
2026-01-04 22:06:35 +0700

    lab4(1+2)

---
+++ src/baitap_javasieutoc/tuhocjava/src/bai26_stringp1.java
@@ -0,0 +1,6 @@
+public class bai26_stringp1 {
+    public static void main(String[] args) {
+        String s = "Hồi đó tôi chê mồm em rộng" + "\nKhông tin hai đứa chập mồm đo";
+        System.out.println(s);
+    }
+}
--- src/src/lab4/lab4_fpt.java
+++ src/src/lab4/lab4_fpt.java
@@ -1,7 +1,46 @@
 package lab4;
 
+import java.util.Scanner;
+
 public class lab4_fpt {
     public static void main(String[] args) {
+        SanPham sp1 = new SanPham();
+        SanPham sp2 = new SanPham();
 
+        System.out.println("Nhập thông tin sản phẩm 1 ");
+        sp1.nhap();
+        System.out.println("Nhập thông tin sản phẩm 2");
+        sp2.nhap();
+        System.out.println("Thông tin sản phẩm: ");
+        sp1.xuat();
+        sp2.xuat();
+
+
+    }
+}
+class SanPham{
+    String tenSpham;
+    double gia;
+    double giamgia;
+
+    public double getThueNhapKhau(){
+        return gia*0.1;
+
+    }
+    public void nhap(){
+        Scanner sc = new Scanner (System.in);
+        System.out.println("Nhập tên sản phẩm: ");
+        tenSpham =sc.nextLine();
+        System.out.println("Nhập đơn giá: ");
+        gia = sc.nextDouble();
+        System.out.println("Nhập giảm giá: ");
+        giamgia= sc.nextDouble();
+
+    }
+    public void xuat(){
+        System.out.println("Tên sản phẩm: "+tenSpham);
+        System.out.println("Đơn giá: "+gia);
+        System.out.println("Giảm giá: "+giamgia);
+        System.out.println("Thuế nhập khẩu: "+getThueNhapKhau());
     }
 }
